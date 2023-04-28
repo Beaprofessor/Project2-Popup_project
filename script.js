c4e66e61c3be05146e3ad93412a874bd0c478a99
Beaprofessor <professornberlin2122@gmail.com>
2023-04-28 22:36:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const openBtn = document.getElementById('open-btn'); 
-const closeBtn = document.getElementById('close-btn'); 
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const popupcontainer = document.querySelector('.popup-container');
+
+openBtn.addEventListener('click', () => {
+
+    popupcontainer.classList.add('blur');
+    popupcontainer.style.display = 'flex';
+})
+
+closeBtn.addEventListener('click', () => {
+
+    popupcontainer.classList.remove('blur');
+    popupcontainer.style.display = 'none';
+
+})
